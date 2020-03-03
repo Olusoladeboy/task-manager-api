d2a58235e61413cbecfdfef322a8f36c51fe59c5
Olusoladeboy <eoyewole508@stu.ui.edu.ng>
2020-03-03 02:31:33 +0100

    Corrected db config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
-const connectionURL = 'mongodb+srv://Olusola:olusola10000@cluster0-lo248.mongodb.net/test';
+const connectionURL = process.env.CONNECTION_URL;
 
 mongoose.connect(connectionURL, {
     useNewUrlParser: true,
